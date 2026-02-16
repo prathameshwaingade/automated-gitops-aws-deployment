a47af43071828a59d373831b239a6252440ab0cb
Prathamesh Waingade <prathmeshwaingade95@gmail.com>
2026-02-16 12:52:50 +0530

    Update app.py

--- automated-gitops-aws-deployment/app/app.py
+++ automated-gitops-aws-deployment/app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Sakshi's Automated GitOps Deployment on AWS EKS!"
+    return "Hello from Prathamesh Automated GitOps Deployment on AWS EKS!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
